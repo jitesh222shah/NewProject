4a7b4954384931553569cb36299e0f56863c36cb
unknown <kigucha1234567@gmail.com>
2022-09-22 20:15:02 +0545

    routing dummy data chnaged and componet pass as element in route

--- src/DummyData/DummyData.jsx
+++ src/DummyData/DummyData.jsx
@@ -1,38 +1,51 @@
-
-const pagesData=[
-    {label:"Home",
-    url:"/",
-    },
-    {
-        label:"Categories",
-        url:"/categories",
-    },
-    {
-        label:"Politics",
-        url:"/Politics",
-    },
-    {
-        label:"Business",
-        url:"/business",
-    },
-    {
-        label:"Health",
-        url:"/health",
-    },
-    {
-        label:"Design",
-        url:"/design",
-    },
-    {
-        label:"Sport",
-        url:"/sport",
-    },
-    {
-        label:"Contact",
-        url:"/contact",
-    },
-    
-
-
+import Home from "../Pages/Home/Home";
+import Categories from "../Pages/Categories/Categories";
+import Politics from "../Pages/Politics/Politics";
+import Business from "../Pages/Business/Business";
+import Contact from "../Pages/Contact/Contact";
+import Health from "../Pages/Health/Health";
+import Design from "../Pages/Design/Design";
+import Sport from "../Pages/Sport/Sport";
+const pagesData = [
+  {
+    component: <Home />,
+    url: "/",
+    label: "Home",
+  },
+  {
+    component: <Categories />,
+    url: "/categories",
+    label: "categories",
+  },
+  {
+    component: <Politics />,
+    url: "/Politics",
+    label: "politics",
+  },
+  {
+    component: <Business />,
+    url: "/business",
+    label: "business",
+  },
+  {
+    component: <Health />,
+    url: "/health",
+    label: "health",
+  },
+  {
+    component: <Design />,
+    url: "/design",
+    label: "design",
+  },
+  {
+    component: <Sport />,
+    url: "/sport",
+    label: "sport",
+  },
+  {
+    component: <Contact />,
+    url: "/contact",
+    label: "contact",
+  },
 ];
-export {pagesData};
+export { pagesData };
--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,47 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import ButtomNavigation from '../Component/Navigation/ButtomNavigation'
-import Footer from '../Component/Foooter/Footer'
-// import Categories from '../Pages/Categories/Categories'
-//  import Home from '../Pages/Home/Home'
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ButtomNavigation from "../Component/Navigation/ButtomNavigation";
+import Footer from "../Component/Foooter/Footer";
+import Categories from "../Pages/Categories/Categories";
+import Home from "../Pages/Home/Home";
 
-// import Politics from '../Pages/Politics/Politics'
-// import Contact from '../Pages/Contact/Contact'
+import Politics from "../Pages/Politics/Politics";
+import Contact from "../Pages/Contact/Contact";
 // import { pagesData } from '../DummyData/DummyData'
 
-const Routing = ({pagesData}) => {
-    const[navData,setNavData]=useState([])
-    useEffect(()=>{
-        setNavData(pagesData)
+const Routing = ({ pagesData }) => {
+    
+  const [navData, setNavData] = useState([]);
+  useEffect(() => {
+    setNavData(pagesData);
+  }, [pagesData]);
+  return (
+    <>
+      <div>
+        <ButtomNavigation pagesData={navData} />
+      </div>
+      <div>
+        {navData.map((each, index) => (
+        <Routes key={index}>
+          <Route path={each?.url} element={each?.component} />
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/politics" element={<Politics />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+         ))} 
+      </div>
 
-    },[pagesData])
-    return (
-        <>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-            <div>
-                <ButtomNavigation pagesData={navData} />
-            </div>
-           <div>
-           {navData.map((each,index) => (
-                <Routes key={index}>
-                    <Route path={each?.url} element={each?.label} />
-                    {/* <Route path="/categories" element={<Categories />} />
-                    <Route path="/politics" element={<Politics />} />
-                    <Route path="/contact" element={<Contact />} /> */}
-
-                </Routes>
-
-            ))}
-           </div>
-
-
-           <div>
-           <Footer />
-           </div>
-
-
-        </>
-    )
-}
-
-export default Routing
+export default Routing;
