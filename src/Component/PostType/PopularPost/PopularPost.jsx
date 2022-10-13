f7bf3334dbce7d376ea6b1da36802044c3814933
unknown <kigucha1234567@gmail.com>
2022-10-13 20:44:18 +0545

    staged changed

---
+++ src/Component/PostType/PopularPost/PopularPost.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Label from '../../Label/Label'
+import './PopularPost.css'
+const PopularPost = ({popularPostData}) => {
+  return (
+    <div>
+        <Label LabelOne='Popular Post'/>
+        {popularPostData.map((each, index)=>(
+            <div className='popularPost-main-container'>
+                <div>
+                    <img className='popular-image-container' src={each.image} alt="" />
+                </div>
+                <div>
+                    <div><h4>{each.title}</h4></div>
+                    <div>{each.imageDetail}</div>
+                </div>
+
+            </div>
+        ))}
+        
+        </div>
+  )
+}
+
+export default PopularPost
